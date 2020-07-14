79ebf8c8d8cb0503d70d8f2017c3f7bd31bf09bb
Marius Andrian <marius.atj@gmail.com>
2020-07-14 20:31:43 +0800

    Updates

---
+++ precache-manifest.be9c14f3813a063fe96b63e859b0043d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac2276afe4a3933e9b89c01447b2589",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "80abc2205920313c51ad",
+    "url": "/movie-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "69a6818fd772d2da7dbc",
+    "url": "/movie-app/static/js/2.9745bae5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.9745bae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80abc2205920313c51ad",
+    "url": "/movie-app/static/js/main.c636960b.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
